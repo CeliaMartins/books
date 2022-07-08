0ee0fd07ea5744c2a803bcd627bba81896305ddd
Célia Martins <celiacmmartins@gmail.com>
2022-07-08 21:32:28 +0100

    2

--- app/Http/Controllers/Frontend/ViewController.php
+++ app/Http/Controllers/Frontend/ViewController.php
@@ -11,27 +11,34 @@ use Illuminate\Http\Request;
 class ViewController extends Controller
 {
 
+
+  //mostra o detalhe da publicação selecionada
+  //identifica o admin que publicou
     public function view_single($id)
     {
-      $post = Book::with('rSubCategory')->where('id', $id)->first();  //mostra o detalhe da publicação selecionada
-      $user_data = Admin::where('id', $post->admin_id)->first();  //identifica o admin que publicou
+      $post = Book::with('rSubCategory')->where('id', $id)->first();
+      $user_data = Admin::where('id', $post->admin_id)->first();
   
       return view('book_details', compact('post', 'user_data'));
     }
 
+    //vai buscar todos os livros e coloca 3 por página
     public function view_list()
     {
-      $all_posts = DB::table('Books')->paginate(3);  //vai buscar todos os livros e coloca 3 por página
+      $all_posts = DB::table('Books')->paginate(3);
               
       return view('book_list', compact('all_posts'));
     }
 
+
+    //pesquisa por titulo
+    //pesquisa por autor
     public function search(Request $request)
     {
       /* dd("pesquisando por {$request->search}"); */
       $filters = $request->all();
-      $all_posts = Book::where('post_title', 'LIKE', "%{$request->search}%")  //pesquisa por titulo
-                        ->orwhere('author', 'LIKE', "%{$request->search}%") //pesquisa por autor
+      $all_posts = Book::where('post_title', 'LIKE', "%{$request->search}%")  
+                        ->orwhere('author', 'LIKE', "%{$request->search}%") 
                         ->paginate();
 
       return view('search_result', compact('filters', 'all_posts'));
